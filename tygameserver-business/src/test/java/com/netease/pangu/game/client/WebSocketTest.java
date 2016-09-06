10cb7aa385d422752a4875692e015e913ca57f04
huangc <hzhuangcheng@corp.netease.com>
2016-09-06 19:39:59 +0800

    update

--- tygameserver-business/src/test/java/com/netease/pangu/game/client/WebSocketTest.java
+++ tygameserver-business/src/test/java/com/netease/pangu/game/client/WebSocketTest.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.Future;
 
 import org.apache.log4j.Logger;
