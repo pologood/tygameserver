a8f025054e752fec89b36ec2470a521ef050b269
huangc <hzhuangcheng@corp.netease.com>
2016-11-04 17:14:01 +0800

    update

--- tygameserver-support/src/main/java/com/netease/pangu/game/http/HttpRequestInvoker.java
+++ tygameserver-support/src/main/java/com/netease/pangu/game/http/HttpRequestInvoker.java
@@ -3,7 +3,6 @@ package com.netease.pangu.game.http;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
