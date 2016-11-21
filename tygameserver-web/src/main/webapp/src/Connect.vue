cc335c7d186db4f608c8454c55d67cb3807d3305
huangc <hzhuangcheng@corp.netease.com>
2016-11-21 14:19:06 +0800

    update

---
+++ tygameserver-web/src/main/webapp/src/Connect.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">连接服务器</h3>
+            </div>
+            <div class="panel-body">
+
+                <div class="form-horizontal">
+
+                     <div class="form-group">
+                        <label class="col-md-2 control-label">玩家名字</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" v-model="player.roleName">
+                        </div>                      
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">uuid</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" v-model="player.uuid">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-md-2 control-label">头像</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" v-model="player.avatarImg">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">房间号</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" v-model="roomId"/> 
+                        </div>
+                    </div>
+
+                    <div class="btncnt form-inline">
+                        <button class="btn btn-success" v-on:click="connectServer">连接服务器</button>
+                    </div>                    
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import $ from 'jquery'
+    import s from './socketManager'
+
+    export default {
+        data(){
+            return{
+                selected:0,
+                roomId:0,
+                playerList:[
+                    {
+                        uuid:'10001',
+                        roleName:'wwt',
+                        avatarImg:'http://img1.360buyimg.com/cms/s244x244_jfs/t1024/361/1267066337/54518/1274eb9a/55973d01N0c07b1af.jpg',
+                        gameId:1
+                    }
+                ],
+                player:{
+                    roleName:'',
+                    avatarImg:'',
+                    uuid:'',
+                    gameId:1
+                }
+            }
+        },
+        methods:{
+            connectServer(){
+                // var player=this.playerList[this.selected];
+                var player = this.player;
+                var self = this;
+                $.getJSON("http://littlegame.tianyu.163.com/master/init?callback=?&uuid="+player.uuid+"&roleName="+player.roleName+"&avatarImg="+encodeURIComponent(player.avatarImg)+"&gameId="+player.gameId+"&roomId="+this.roomId, function(data){
+                    s.roomId = self.roomId;
+                    s.player = player;
+                    s.avatarId=data.avatarId;
+                    s.connectSocket(data);
+                })
+            }
+        }
+    }
+</script>
+<style scoped>
+    .panel{width: 600px;margin: 0 auto;margin-top: 300px;}
+    .btncnt{text-align: center;margin-top: 10px;}
+    .form-group{margin-bottom: 20px;}
+</style>
---
+++ tygameserver-web/src/main/webapp/src/socketManager.js
@@ -0,0 +1,242 @@
+export default{
+    rootVue:null,
+    connectData:null,
+    socket:null,
+    router:null,
+    roomId:0,
+    avatarId:0,
+    player:{
+        roleName:''
+    },
+    count:0,
+    ownerName:'',
+    state:0,
+    maxSize:0,
+    members:{},
+    groupmsgs:[],
+    painterId:0,
+    border:null,
+    hint:{
+        hint1:'',
+        hint2:''
+    },
+    answerList:[],
+    connectSocket(data){
+        const self =this;
+        // 创建一个Socket实例		
+        this.connectData=data;
+		this.socket = new WebSocket('ws://' + this.connectData.ip + ':'+this.connectData.port+'/websocket'); 
+
+        // 打开Socket 
+		this.socket.onopen = function(event) { 
+            console.log('Client open a message',event.data); 
+            self.router.push('create');
+        };
+         // 监听消息
+        this.socket.onmessage = function(event) { 
+            console.log('Client received a message', event.data);
+            data = JSON.parse(event.data);
+            if(data.content.code == 1){
+                // if(data.rpcMethodName.toLowerCase() == "/player/reg") {
+                //     self.sessionId=data.content.payload.sessionId.toString();
+                //     self.userName=data.content.payload.roleName;
+                // }
+
+                // if(data.rpcMethodName.toLowerCase() == "/player/login") {
+                //     self.sessionId=data.content.payload.sessionId;
+                //     self.userName=data.content.payload.roleName;
+                // }
+
+                // if(data.rpcMethodName.toLowerCase() == "/player/chat") {
+                //     self.msgList.push(data.content.source.playerName+" "+data.content.payload.msg);
+                // }
+
+                // if(data.rpcMethodName.toLowerCase() == "/player/list") {
+                //     console.log(data.content.payload);
+                //     self.playerList=data.content.payload;
+                // }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/create"){
+                    console.log(data.content.payload);
+                    self.roomId=data.content.payload;	
+                    self.router.push('room');							
+                    // alert("创建房间" + data.content.payload);
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/join"){
+                    console.log(data.content.payload);
+                    self.roomId=data.content.payload;
+                    self.router.push('room');	
+                    // alert("加入房间" + data.content.payload);
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/info"){
+                    console.log(data.content.payload);					
+                    // self.members = data.content.payload.members;
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/broadcast/roominfo"){
+                    console.log(data.content.payload);	
+                    self.ownerName = data.content.payload.ownerName;
+                    self.members = data.content.payload.members;
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/avatar/ready"){
+                    self.state=1;
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/chat"){
+                    console.log(data.content.payload);	
+                    self.groupmsgs.push({msg:data.content.payload.msg,from:data.content.source.avatarName});
+                }
+
+                if(data.rpcMethodName.toLowerCase() ==  "/guess/create"){
+                    
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/broadcast/startgame"){
+                    self.painterId = data.content.payload;
+                    self.router.push('draw');
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/broadcast/drawgame"){
+                    var info=data.content.payload;
+                    if(info.type==1){
+                        self.border.drawing(info.drawInfo);
+                    }else if(info.type==2){
+                        self.border.clear();
+                    }
+                    
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/broadcast/drawquestion"){
+                    self.hint=data.content.payload;
+                }
+
+                if(data.rpcMethodName.toLowerCase() == "/room/broadcast/answer"){
+                    self.answerList.push(data.content.payload);
+                }
+
+                // if(data.rpcMethodName.toLowerCase() == "/room/list"){
+                //     console.log(data.content.payload);
+                //     self.roomList = data.content.payload;
+                // }
+
+            }else{
+                console.log(data.content.payload);
+                // alert(data.message);
+            }
+
+        };
+
+        // 监听Socket的关闭
+        this.socket.onerror = function(event) { 
+            console.log('Client notified socket has error',event.data); 
+        }; 
+        // 监听Socket的关闭
+        this.socket.onclose = function(event) { 
+            console.log('Client notified socket has closed',event.data); 
+        };
+
+    },
+    createRoom(){
+        console.log('createRoom')
+        var msg = {
+            rpcMethod:"/room/create", 
+            params:{
+                gameId:this.player.gameId,
+                maxSize:10
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    },
+    joinRoom(){
+        console.log('joinRoom')
+        var msg = {
+            rpcMethod:"/room/join", 
+            params:{
+                roomId:this.roomId
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    },
+    ready(){
+        console.log('ready')
+        var msg = {
+            rpcMethod:"/avatar/ready", 
+            params:{
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    },
+    sendMsg(msg){
+        console.log('sendMsg')
+        var msg = {
+            rpcMethod:"/room/chat", 
+            params:{
+                roomId:this.roomId,
+                msg:msg
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    },
+    startGame(){
+        console.log('startGame')
+        var msg = {
+            rpcMethod:"/guess/create", 
+            params:{
+                roomId:this.roomId
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    },
+    sendDrawingInfo(info){
+        var msg = {
+            rpcMethod:"/guess/draw", 
+            params:{
+                roomId:this.roomId,
+                content:info
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    },
+    sendQuestion(info){
+        var msg = {
+            rpcMethod:"/guess/question", 
+            params:{
+                roomId:this.roomId,
+                answer:info.word,
+                hint1:info.hint1,
+                hint2:info.hint2
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    },
+    sendAnswer(word){
+        var msg = {
+            rpcMethod:"/guess/answer", 
+            params:{
+                roomId:this.roomId,
+                answer:word
+            },
+            gameId:this.player.gameId,
+            uuid:this.player.uuid
+        };
+        this.socket.send(window.JSON.stringify(msg));
+    }
+
+}
