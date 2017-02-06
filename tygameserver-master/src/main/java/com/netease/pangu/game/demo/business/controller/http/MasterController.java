e774e3df174e07706266be168d4585563ebe8a72
huangc <hzhuangcheng@corp.netease.com>
2017-02-06 14:54:40 +0800

    misc

--- tygameserver-master/src/main/java/com/netease/pangu/game/demo/business/controller/http/MasterController.java
+++ tygameserver-master/src/main/java/com/netease/pangu/game/demo/business/controller/http/MasterController.java
@@ -8,7 +8,6 @@ import com.netease.pangu.game.http.annotation.HttpController;
 import com.netease.pangu.game.http.annotation.HttpRequestMapping;
 import com.netease.pangu.game.meta.Avatar;
 import com.netease.pangu.game.meta.DataCenterSimpleRoleInfo;
-import com.netease.pangu.game.meta.GuessQuestion;
 import com.netease.pangu.game.service.AvatarService;
 import com.netease.pangu.game.service.DataCenterApiService;
 import com.netease.pangu.game.service.GuessGameService;
@@ -120,10 +119,5 @@ public class MasterController {
             avatarObjList.add(avatarObj);
         }
         return avatarObjList;
-    }
-
-    @HttpRequestMapping("/questions")
-    public List<GuessQuestion> getQuestions() {
-        return guessGameService.getQuestions();
     }
 }
