86aa43f94f7c932891aab1ed70dd0fc8ee8f9768
huangc <hzhuangcheng@corp.netease.com>
2017-02-15 17:00:30 +0800

    misc

--- tygameserver-slave/slave.sh
+++ tygameserver-slave/slave.sh
@@ -2,5 +2,5 @@
 echo start to run slave
 nohup java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8700,suspend=n -jar target/tygameserver-slave-0.0.1-SNAPSHOT.jar > slave.log 2>&1 &
 PID=$!
-echo $PID > master.pid
+echo $PID > slave.pid
 tail -f slave.log
